a1fdaa6df8d368fef123ca1f81c60c556dedc0a1
Terence Zengeya <terencetamuka@gmail.com>
2024-08-07 17:09:01 +0000

    Added the loop function for all pages to work accordinlgy

--- end.js
+++ end.js
@@ -1,35 +1,37 @@
-const username = document.getElementById('username');
-const saveScoreBtn = document.getElementById('#saveScoreBtn');
-const finalScore = document.getElementById('#finalScore');
-const mostRecentScore = document.getElementById('#mostRecentScore');
-
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5
 
-finalScore.innerText = mostRecentScore;
+finalScore.innerText = mostRecentScore
 
-username.addEventListener('keup', () => {
-    saveScoreBtn.disabled = !username.value;
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value
 })
 
-saveHighScore = e => {
-
+const saveHighScore = e => {
     e.preventDefault()
+
     const score = {
         score: mostRecentScore,
         name: username.value
     }
 
-    highScores.push(score)
+    highScores.push(score);
 
     highScores.sort((a, b) => {
-        return b.score - a.score
+        return b.score - a.score;
     })
 
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('/highscores.html')
+    window.location.assign('/')
 }
+
+// Attach the saveHighScore function to the saveScoreBtn click event
+// saveScoreBtn.addEventListener('click', saveHighScore);
